d0872aec75c048d078b7459fe80d3cd24d75f475
Daniel Kim <khs12100@gmail.com>
2023-04-22 14:31:53 -0400

    init2

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<div class="flex flex-col justify-center items-center">
+	<div class="flex justify-center mt-10">
+		<p class="unstyled text-5xl ">AI-Generated Trivia Game</p>
+	</div>
+	<div class="flex flex-col justify-center mt-5">
+		<p>Which League of Legends Champion is...</p>
+		<textarea class="textarea mt-1" cols="32" rows="2" placeholder="" />
+		<button type="button" class="btn variant-filled mt-4">Generate</button>
+	</div>
+	<div class="grid grid-cols-4 gap-2  mt-8">
+		<div class="w-60 h-[18rem] bg-slate-900 flex justify-center">
+			<p>
+				Yasuo
+			</p>
+		</div>
+		<div class="w-60 h-[18rem] bg-slate-900">
+		</div>
+		<div class="w-60 h-[18rem] bg-slate-900">
+		</div>
+		<div class="w-60 h-[18rem] bg-slate-900">
+		</div>
+
+	</div>
+</div>
